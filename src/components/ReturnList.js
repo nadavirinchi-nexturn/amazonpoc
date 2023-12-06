fde50849c84d3073eaa0afd7114251755b56ef82
NadaVirinchi_v <nada.virinchi@nexturn.com>
2023-12-06 19:32:11 +0530

    list changes 2.

--- src/components/ReturnList.js
+++ src/components/ReturnList.js
@@ -6,11 +6,11 @@ import { useNavigate } from 'react-router-dom';
 
 const ReturnList = () => {
 
-  const naigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <Box sx={{ width: '100vw', height: '100vh', overflowX: 'hidden' }}>
-        <Box sx={{ height: '30%', background: '#131921', width: '100%', paddingTop: '1%' }}>
+        <Box sx={{ height: '40%', background: '#131921', width: '100%', paddingTop: '1%' }}>
           <Stack direction='row' alignItems='center' justifyContent='space-around'>
             <Typography fontFamily='Gilroy' color='white' fontWeight='1000'>Returns RAD</Typography>
             <TextField 
@@ -123,13 +123,80 @@ const ReturnList = () => {
                 height: '40px',
                 color: '#131921'
               }}
-              onClick={() => naigate('/create')}
+              onClick={() => navigate('/create')}
             >+ New Request</Button>
           </Stack>
           <Stack direction='row' alignItems='center' height='40px' sx={{ background: 'white', margin: '1%', borderRadius: '5px' }}>
             <Typography fontFamily='Gilroy' color='#131921' fontWeight='1000' marginLeft='2%'>Approved</Typography>
             <Typography fontFamily='Gilroy' color='#131921' fontWeight='1000' marginLeft='2%'>Submitted</Typography>
             <Typography fontFamily='Gilroy' color='#131921' fontWeight='1000' marginLeft='2%'>Draft</Typography>
+          </Stack>
+          <Stack direction='row' alignItems='center' margin='1%'>
+            <Stack 
+              direction='row' 
+              width='100px' 
+              sx={{ background: 'white' }} 
+              justifyContent='space-around' 
+              padding='10px' 
+              borderRadius='20px'
+            >
+              <Typography fontFamily='Gilroy' color='#131921'>ALL</Typography>
+              <Box sx={{ 
+                width: '40px', 
+                borderRadius: '10px', 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center',
+                background: '#E6EEFF' 
+              }}
+              >
+                <Typography fontFamily='Gilroy' color='#131921'>99+</Typography>
+              </Box>
+            </Stack>
+            <Stack 
+              direction='row' 
+              width='100px' 
+              sx={{ background: 'white' }} 
+              justifyContent='space-around' 
+              padding='10px' 
+              borderRadius='20px'
+              marginLeft='1%'
+            >
+              <Typography fontFamily='Gilroy' color='#131921'>Return</Typography>
+              <Box sx={{ 
+                width: '40px', 
+                borderRadius: '10px', 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center',
+                background: '#E6EEFF' 
+              }}
+              >
+                <Typography fontFamily='Gilroy' color='#131921'>70</Typography>
+              </Box>
+            </Stack>
+            <Stack 
+              direction='row' 
+              width='100px' 
+              sx={{ background: 'white' }} 
+              justifyContent='space-around' 
+              padding='10px' 
+              borderRadius='20px'
+              marginLeft='1%'
+            >
+              <Typography fontFamily='Gilroy' color='#131921'>Draft</Typography>
+              <Box sx={{ 
+                width: '40px', 
+                borderRadius: '10px', 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center',
+                background: '#E6EEFF' 
+              }}
+              >
+                <Typography fontFamily='Gilroy' color='#131921'>40</Typography>
+              </Box>
+            </Stack>
           </Stack>
         </Box>
     </Box>
