3637d6f52ba3d1ebbf802619891afe331e72e97a
NadaVirinchi_v <nada.virinchi@nexturn.com>
2023-12-06 18:51:53 +0530

    naigate changes..

--- src/components/ReturnList.js
+++ src/components/ReturnList.js
@@ -2,8 +2,12 @@ import { Box, Button, InputAdornment, Stack, TextField, Typography } from '@mui/
 import React from 'react'
 import SearchIcon from "@mui/icons-material/Search";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { useNavigate } from 'react-router-dom';
 
 const ReturnList = () => {
+
+  const naigate = useNavigate();
+
   return (
     <Box sx={{ width: '100vw', height: '100vh', overflowX: 'hidden' }}>
         <Box sx={{ height: '30%', background: '#131921', width: '100%', paddingTop: '1%' }}>
@@ -105,6 +109,27 @@ const ReturnList = () => {
                 width: '100px'
               }}
             >Search</Button>
+          </Stack>
+          <Stack direction='row' alignItems='center' justifyContent='space-between' padding='1%'>
+            <Typography fontFamily='Gilroy' color='white'>Return List</Typography>
+            <Button
+              sx={{
+                background: '#FFBB5C',
+                textTransform: 'none',
+                '&:hover': {
+                  background: '#FFBB5C'
+                },
+                width: '200px',
+                height: '40px',
+                color: '#131921'
+              }}
+              onClick={() => naigate('/create')}
+            >+ New Request</Button>
+          </Stack>
+          <Stack direction='row' alignItems='center' height='40px' sx={{ background: 'white', margin: '1%', borderRadius: '5px' }}>
+            <Typography fontFamily='Gilroy' color='#131921' fontWeight='1000' marginLeft='2%'>Approved</Typography>
+            <Typography fontFamily='Gilroy' color='#131921' fontWeight='1000' marginLeft='2%'>Submitted</Typography>
+            <Typography fontFamily='Gilroy' color='#131921' fontWeight='1000' marginLeft='2%'>Draft</Typography>
           </Stack>
         </Box>
     </Box>
